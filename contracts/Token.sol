9f165e2bbd43c9776cc4e62770f54ef0abfe6776
bipin parmar <parmarbipin96013@gmail.com>
2022-11-14 22:54:21 +0530

    transfer token and approve token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,9 +11,54 @@ contract Token {
 
     uint public totalSupply = 1000000 * (10**decimals);
 
+    mapping(address => uint) public balanceOf;
+
+    mapping(address => mapping(address => uint)) public allowance;
+
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint value
+    );
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint value
+    );
+
     constructor(string memory _name, string memory _symbol){
         name = _name;
         symbol = _symbol;
+
+        balanceOf[msg.sender] = totalSupply;
     }
 
+    function transfer(address _to, uint _amount) public returns(bool success){
+        // require that sender has enough tokens to spend
+        require(balanceOf[msg.sender] >= _amount);
+
+        // reject invalid
+        require(_to != address(0));
+
+        // deduct token from sender
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _amount;
+        //create tokens to receiver
+        balanceOf[_to] = balanceOf[_to] + _amount;
+
+        emit Transfer(msg.sender, _to, _amount);
+
+        return true;
+
+    }
+
+    function approve(address _spender, uint _value) public returns(bool success){
+        // reject invalid
+        require(_spender != address(0));
+
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
 }
--- test/Token.js
+++ test/Token.js
@@ -7,16 +7,23 @@ const tokens = (n) => {
 describe('Token', () => {
 
     let token
-
+    let accounts
+    let deployer
+    let receiver
+    let exchange
     beforeEach(async () => {
 
         // Fetch token from Blockchain
         const Token = await ethers.getContractFactory('Token')
         token = await Token.deploy('Bipin Parmar', 'BIPS')
 
+        accounts = await ethers.getSigners()
+        deployer = accounts[0]
+        receiver = accounts[1]
+        exchange = accounts[2]
     })
 
-    describe('deployment', () => {
+    describe('Deployment', () => {
         const name = "Bipin Parmar"
         const symbol = 'BIPS'
         const decimals = 18
@@ -37,7 +44,109 @@ describe('Token', () => {
         it('has correct total supply', async () => {
             expect(await token.totalSupply()).to.equal(totalSupply)
         })
+
+        it('assign total supply to deployer', async () => {
+            expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+        })
+    })
+
+    describe('Sending tokens' , ()=>{
+        describe('success' , ()=>{
+
+            let transaction, result, amount
+            beforeEach(async () => {
+
+                amount = tokens(100)
+                // transfer token
+                transaction =  await token.connect(deployer).transfer(receiver.address, tokens(100))
+                result = await transaction.wait()
+            })
+
+            it('transfer token balances', async ()=>{
+
+                // console.log('deployer balance', await token.balanceOf(deployer.address))
+                // console.log('receiver balance', await token.balanceOf(receiver.address))
+
+                // ensure that tokens were transfer to receiver.
+                expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+                expect(await token.balanceOf(receiver.address)).to.equal(amount)
+
+                // console.log('deployer balance',await token.balanceOf(deployer.address))
+                // console.log('receiver balance',await token.balanceOf(receiver.address))
+            })
+
+
+            it('emits a transfer event', async ()=>{
+
+                const event = result.events[0];
+                const arg = event.args
+
+                expect(event.event).to.equal('Transfer')
+
+                expect(arg.from).to.equal(deployer.address)
+                expect(arg.to).to.equal(receiver.address)
+                expect(arg.value).to.equal(amount)
+            })
+
+
+        })
+
+        describe('failure' , ()=>{
+
+            it('reject insufficient balances', async ()=>{
+
+                // transfer more tokens than deployer has 100
+                const invalidAmount = tokens(100000000)
+                await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+            })
+            it('reject invalid address', async ()=>{
+
+                // transfer more tokens than deployer has 100
+                const amount = tokens(10)
+                await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+            })
+        })
+
     })
 
 
+    describe('Approving tokens' , ()=>{
+
+        let transaction, result, amount
+        beforeEach(async () => {
+
+            amount = tokens(100)
+            // transfer token
+            transaction =  await token.connect(deployer).approve(exchange.address, amount)
+            result = await transaction.wait()
+        })
+
+        describe('Success' , ()=>{
+
+            it('allocates an allowance for delegated token spending', async ()=>{
+                expect(await token.allowance(deployer.address, exchange.address)).to.equal(amount)
+            })
+
+            it('emits a approval event', async ()=>{
+
+                const event = result.events[0];
+                const arg = event.args
+
+                expect(event.event).to.equal('Approval')
+
+                expect(arg.owner).to.equal(deployer.address)
+                expect(arg.spender).to.equal(exchange.address)
+                expect(arg.value).to.equal(amount)
+            })
+        })
+
+        describe('Failure' , ()=>{
+
+            it('reject invalid spenders', async ()=>{
+                await expect( token.connect(deployer).approve('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+            })
+
+        })
+
+    })
 })
